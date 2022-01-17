2caf2acac85d390d53c85024809cfd2314b20c40
ChibyKito <maxi-trenz@web.de>
2022-01-17 10:01:07 +0100

    Commite dieses Ding

---
+++ VarRectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace grafikprogramm_net_core
+{
+    public class VarRectangle
+    {
+
+        //BLIBLABLUB Wir fahren ins Kabuff
+    }
+}
